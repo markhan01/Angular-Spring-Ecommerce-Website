42cefd4052c8ab060a2209f37fbf7f2b283a99b5
markhan01 <hanmarkw2001@gmail.com>
2023-04-10 19:01:33 -0400

    implemented ci/cd github actions workflow to automatically deploy angular code to AWS S3

---
+++ frontend/angular-ecommerce/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  shopApiUrl: "http://angularspringecommerce-env.eba-erdsh2ir.us-east-2.elasticbeanstalk.com/api",
+  stripePublishableKey: "pk_test_51MVKciCgU7OdoaDUpBUAfUC1MrCZm4fO96tqwEpCBb6fS0kYp316tmnKYIR3r16h9RZJQJgTy8gYTedlQAvInhaS00rvn5fFFW"
+};
